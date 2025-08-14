1bf762ddb44eb9e9612c07cbccfbff4e0b85343b
Jeneral1 <reflectical@gmail.com>
2025-08-14 21:20:14 +0000

    Added authententication + layout

--- teammove/move/urls.py
+++ teammove/move/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('', views.home, name="Bloghome"),
+
+    path('login/', auth_views.LoginView.as_view(template_name='move/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='move/password_reset.html'), name='password_reset'),
+    path('signup/', views.signup, name='signup'),
 ]
--- teammove/move/views.py
+++ teammove/move/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 # Create your views here.
 
 def home(request):
     return render(request, 'move/index.html')
     
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('Bloghome')
+    else:
+        form = UserCreationForm()
+    return render(request, 'move/signup.html',{'form': form})
